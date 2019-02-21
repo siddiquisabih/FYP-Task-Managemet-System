6e1b58030a4527e26a82ee8664c4671db210f1a2
sabih <siddiquisabih94@gmail.com>
2019-02-21 13:59:41 +0500

    change local and live link

--- client/src/Constants/constants.js
+++ client/src/Constants/constants.js
@@ -5,8 +5,8 @@
  class Constant {
 
     
-    // static BASE_URL = 'https://patient-server.herokuapp.com/' //live
-    static BASE_URL = 'http://10.30.1.108:3050/' //local
+    static BASE_URL = 'https://patient-server.herokuapp.com/' //live
+    // static BASE_URL = 'http://10.30.1.108:3050/' //local
 
     static AUTH_LOCAL_KEY = 'auth'
 

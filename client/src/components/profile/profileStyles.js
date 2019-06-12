2f4943e3f73d5e1b5574b39b7f73f8758c0e4df5
sabih <siddiquisabih94@gmail.com>
2019-06-12 17:06:29 +0500

    half user profile

--- client/src/components/profile/profileStyles.js
+++ client/src/components/profile/profileStyles.js
@@ -9,4 +9,20 @@ export default StyleSheet.create({
     header: {
         backgroundColor: "#039be5"
     },
+
+    gradientBox: {
+        width: 100,
+        height: 100,
+        borderRadius: 100 / 2,
+    },
+
+    pendingCard: {
+        width: 100,
+        height: 100,
+        borderRadius: 100 / 2,
+        // marginLeft: '5%',
+        marginTop: '5%',
+        // borderRadius: 50,
+
+    },
 })
--- client/src/components/profile/userProfile.js
+++ client/src/components/profile/userProfile.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { View, Image } from 'react-native'
 import { Button, Content, Body, Container, Header, Title, Card, Right, Text, Left, Icon, Tab, Tabs, TabHeading } from 'native-base';
 import styles from './profileStyles'
 class UserProfile extends Component {
@@ -29,6 +29,52 @@ class UserProfile extends Component {
                     </Body>
                     <Right />
                 </Header>
+
+
+                <Content>
+
+
+
+
+
+
+                    {/* <Image style={{ width: '20%', height: "20%" }} source={require("../../images/copy.jpg")} /> */}
+
+
+                    <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+
+
+
+                        <Card style={styles.pendingCard}>
+                            <View style={styles.gradientBox}  >
+
+                                {/* <Image  source={require("../../images/copy.jpg")} /> */}
+                                <View style={{ justifyContent: 'center' }}>
+                                    <Text>asd</Text>
+                                </View>
+
+                            </View>
+                        </Card>
+
+
+
+                    </View>
+
+
+
+
+
+
+
+
+
+
+
+
+
+                </Content>
+
+
 
             </Container>
         )

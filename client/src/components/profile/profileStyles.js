0b9502ce49d714a2a4f12e8854698526717caed8
sabih <siddiquisabih94@gmail.com>
2019-07-02 19:01:14 +0500

    profile added

--- client/src/components/profile/profileStyles.js
+++ client/src/components/profile/profileStyles.js
@@ -25,4 +25,59 @@ export default StyleSheet.create({
         // borderRadius: 50,
 
     },
+
+    userImage: {
+        width: 100,
+        height: 100,
+        borderRadius: 100 / 2,
+        resizeMode: 'cover'
+    },
+    userName: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: 'white'
+    },
+
+    gridCol: {
+        flexDirection: 'row',
+        height: 100,
+        padding: 20,
+        margin: 20,
+        borderRadius: 10,
+        borderWidth: 0.5,
+        borderColor: 'white',
+
+    },
+    gridCell: {
+        flex: 0.3,
+        justifyContent: 'center',
+         borderRightWidth: 0.5,
+         borderColor: 'white',
+        // borderRadius: 10,
+        // borderWidth: 0.5,
+        // marginRight:10
+    },
+    gridCellValue: {
+        flex: 1,
+        justifyContent: 'center',
+        // borderRadius: 10,
+        // borderWidth: 0.5,
+        // borderColor: 'white',
+        // marginRight:10
+       
+        
+    },
+    gridText: {
+        // paddingLeft: 8,
+        color:'white',
+    },
+    gridTextvalue: {
+        // paddingRight: 10,
+        textAlign: 'right',
+        color:'white'
+    },
+
+
+
+
 })
--- client/src/components/profile/userProfile.js
+++ client/src/components/profile/userProfile.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { View, Image } from 'react-native'
 import { Button, Content, Body, Container, Header, Title, Card, Right, Text, Left, Icon, Tab, Tabs, TabHeading } from 'native-base';
 import styles from './profileStyles'
+import LinearGradient from 'react-native-linear-gradient';
+
 class UserProfile extends Component {
     static navigationOptions = {
         title: "Profile",
@@ -30,35 +32,86 @@ class UserProfile extends Component {
                     <Right />
                 </Header>
 
+                <LinearGradient colors={['#b3e5fc', '#03a9f4', '#039be5']} style={{ flex: 1 }}>
 
-                <Content>
-
-
-
+                    <Content>
 
 
 
-                    {/* <Image style={{ width: '20%', height: "20%" }} source={require("../../images/copy.jpg")} /> */}
-
-
-                    <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
 
 
 
-                        <Card style={styles.pendingCard}>
-                            <View style={styles.gradientBox}  >
+                        {/* <Image style={{ width: '20%', height: "20%" }} source={require("../../images/copy.jpg")} /> */}
 
-                                {/* <Image  source={require("../../images/copy.jpg")} /> */}
-                                <View style={{ justifyContent: 'center' }}>
-                                    <Text>asd</Text>
+
+                        <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+
+
+
+                            <Card style={styles.pendingCard}>
+                                <View style={styles.gradientBox}  >
+                                    <Image style={styles.userImage} source={require("../../images/copy.jpg")} />
                                 </View>
+                            </Card>
+                        </View>
 
+
+                        <View><Text style={styles.userName} >Sabih Siddiqui</Text></View>
+                        <View><Text style={styles.userName} >Application Developer</Text></View>
+
+
+
+
+
+
+                        <View
+                            style={styles.gridCol}>
+                            <View style={styles.gridCell}>
+                                <Text style={styles.gridText} >Email</Text>
                             </View>
-                        </Card>
+                            <View style={styles.gridCellValue}>
+                                <Text style={styles.gridTextvalue} >SiddiquiSabih94@gmail.com</Text>
+                            </View>
+
+
+                        </View>
+                       
+                        <View
+                            style={styles.gridCol}>
+                            <View style={styles.gridCell}>
+                                <Text style={styles.gridText} >Status</Text>
+                            </View>
+                            <View style={styles.gridCellValue}>
+                                <Text style={styles.gridTextvalue} >Intern</Text>
+                            </View>
+
+
+                        </View>
+
+                        <View
+                            style={styles.gridCol}>
+                            <View style={styles.gridCell}>
+                                <Text style={styles.gridText} >Joining</Text>
+                            </View>
+                            <View style={styles.gridCellValue}>
+                                <Text style={styles.gridTextvalue} >24-May-2018</Text>
+                            </View>
+                        </View>
+
+                        <View
+                            style={styles.gridCol}>
+                            <View style={styles.gridCell}>
+                                <Text style={styles.gridText} >Salary</Text>
+                            </View>
+                            <View style={styles.gridCellValue}>
+                                <Text style={styles.gridTextvalue} >65,000</Text>
+                            </View>
+
+
+                        </View>
 
 
 
-                    </View>
 
 
 
@@ -66,14 +119,8 @@ class UserProfile extends Component {
 
 
 
-
-
-
-
-
-
-                </Content>
-
+                    </Content>
+                </LinearGradient>
 
 
             </Container>
